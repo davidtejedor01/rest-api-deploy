c4be52a96947fc8a20fa5d5bda38c749216c0d2b
David Tejedor <dtejedor18@gmail.com>
2025-04-03 03:58:18 -0300

    Arquitectura MVC 1.0

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,4 +1,4 @@
-const z = require("zod");
+import z from 'zod'
 
 const movieSchema = z.object({
   title: z.string({
@@ -30,11 +30,10 @@ const movieSchema = z.object({
   rate: z.number().min(0).max(10).default(5),
 });
 
-function validateMovie(object) {
+export function validateMovie(object) {
   return movieSchema.safeParse(object);
 }
-function validatePartial(object) {
+export function validatePartial(object) {
   return movieSchema.partial().safeParse(object);
 }
 
-module.exports = { validateMovie, validatePartial };
